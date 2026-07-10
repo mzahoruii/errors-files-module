5d414b2ac9dbd48947754a9d12c8f4d03d294be7
Mariia Zahorui <mariia.zahorui.25@kse.org.ua>
2026-07-10 12:38:47 +0300

    Live Coding 2: Робота з JSON API форматами

--- files_practice.py
+++ files_practice.py
@@ -2,24 +2,36 @@ import csv
 import json
 
 # Ваш код для завдань нижче:
-import csv
+# import csv
+#
+# valid_transactions = []
+# with open("files/transactions.csv", "r", encoding="utf-8") as file:
+#     reader = csv.DictReader(file)
+#     suma=0
+#     for row in reader:
+#         if row["status"]=="success":
+#             try:
+#                 suma += float( row["amount"])
+#                 valid_transactions.append(row)
+#             except:
+#                 print(f"Unsupported value {row['amount']}")
+# print(suma)
+#
+# with open ("files/valid_transactions.csv", "w", encoding="utf-8", newline='') as file:
+#     writer =csv.DictWriter(file, fieldnames=valid_transactions[0].keys())
+#     writer.writeheader()
+#     writer.writerows(valid_transactions)
 
-valid_transactions = []
-with open("files/transactions.csv", "r", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-    suma=0
-    for row in reader:
-        if row["status"]=="success":
-            try:
-                suma += float( row["amount"])
-                valid_transactions.append(row)
-            except:
-                print(f"Unsupported value {row['amount']}")
-print(suma)
+# users_new=[]
+# with open ("files/users.json", "r", encoding="utf-8" ) as f:
+#     users=json.load(f)
+#     print(users)
+#
+#     for user in users:
+#         if user["balance"]>1000:
+#             users_new.append({"id": user["id"], "balance": user["balance"], "status": user.get("VIP", "VIP")})
+#         else:
+#             users_new.append({"id": user["id"], "balance" : user["balance"], "status" : "regular"})
+# print(users_new)
 
-with open ("files/valid_transactions.csv", "w", encoding="utf-8", newline='') as file:
-    writer =csv.DictWriter(file, fieldnames=valid_transactions[0].keys())
-    writer.writeheader()
-    writer.writerows(valid_transactions)
 
-    
